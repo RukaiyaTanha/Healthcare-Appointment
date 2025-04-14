ddff5fff0cc05ee66db05c459c22b012c020ad87
Rukaiya <ahnattanha562@gmail.com>
2025-04-14 09:55:15 +0600

    Form Submitted

---
+++ view.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>View</title>
+    <link rel="stylesheet" href="doctor.css">
+</head>
+<body>
+
+        <h2>Welcome Doctor! This is the empty doctor page.</h2>
+</body>
+</html>
